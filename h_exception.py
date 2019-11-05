d4f7e3060110b1d9d4d6bd2fcad848a1d11f87f2
jackyin2 <956486535@qq.com>
2019-11-05 17:35:01 +0800

    新增mysqlerror

--- h_exception.py
+++ h_exception.py
@@ -13,7 +13,6 @@ class MyExcepiton(Exception):
         return ("异常：{}".format(
             self.__class__.__name__))
 
-
 class JsonError(MyExcepiton):
     """
     所有Json相关的错误父类
@@ -21,24 +20,20 @@ class JsonError(MyExcepiton):
     def __init__(self, j=None):
         self.j = j
 
-
 class ParseJsonError(JsonError):
     def __str__(self):
         return ("exception：{}, message：parse json {} error ".format(
             self.__class__.__name__, self.j))
-
 
 class NotJsonError(JsonError):
     def __str__(self):
         return ("exception：{}, message：{} is not json ".format(
             self.__class__.__name__, self.j))
 
-
 class LoadJsonFileError(JsonError):
     def __str__(self):
         return ("exception：{}, message：from file:{} load api error".format(
             self.__class__.__name__, self.j))
-
 
 class NotEqualError(MyExcepiton):
     def __init__(self, a=None, b=None, m=None):
@@ -50,7 +45,6 @@ class NotEqualError(MyExcepiton):
         return ("exception：{}, method:{},  message:{}  ！=  {}".format(
             self.__class__.__name__, self.m, self.a, self.b))
 
-
 class NotContainError(MyExcepiton):
     def __init__(self, a=None, b=None, m=None):
         self.a = a
@@ -61,7 +55,6 @@ class NotContainError(MyExcepiton):
         return ("exception：{}, method:{},  message:{} not found {}".format(
             self.__class__.__name__, self.m, self.a, self.b))
 
-
 class NotFoundParams(MyExcepiton):
     def __init__(self, p=None):
         self.p = p
@@ -70,15 +63,12 @@ class NotFoundParams(MyExcepiton):
         return ("exception：{}, message:not found params {}".format(
             self.__class__.__name__, self.p))
 
-
 class ParamsError(MyExcepiton):
     def __init__(self, p=None):
         self.p = p
 
     def __str__(self):
         return ("{}".format(self.p))
-
-
 
 class EvalError(MyExcepiton):
     def __init__(self, p=None, m=None):
@@ -89,7 +79,6 @@ class EvalError(MyExcepiton):
         return ("exception:{}, method:{}, message:eval {} error".format(
             self.__class__.__name__, self.m,  self.p))
 
-
 class RequestError(MyExcepiton):
     def __init__(self, a=None):
         self.a = a
@@ -97,7 +86,6 @@ class RequestError(MyExcepiton):
     def __str__(self):
         return ("exception：{}, message： {}".format(
             self.__class__.__name__, self.a))
-
 
 class TimeOutError(RequestError):
     def __str__(self):
